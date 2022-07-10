a65f85f932be1d82f8d239cc7d19f5d5aa6ef24b
donnutdev <77726912+donnutdev@users.noreply.github.com>
2022-07-10 20:40:40 +0530

    final commit for code

---
+++ website/staticdata.py
@@ -0,0 +1,43 @@
+POSTS = [
+    "head_boy",
+    "head_girl",
+    "vice_head_boy",
+    "vice_head_girl",
+    "sports_captain",
+    "vice_sports_captain",
+    "cultural_head",
+    "vice_cultural_head",
+    "house_captain_spartans",
+    "house_captain_samurais",
+    "house_captain_trojans",
+    "house_captain_knights",
+    "vice_house_captain_spartans",
+    "vice_house_captain_samurais",
+    "vice_house_captain_trojans",
+    "vice_house_captain_knights",
+]
+
+CANDIDATES = ["Hanif Hasan Patel", "Aarush Verma", "Diya Mahajan", "Sadhvi Kumar", "Shiven Onsker", "Shlok Rautwar",
+              "Saanvi Trivedi", "Amaira Achwa", "Kyle Daniel", "Siddhant Dey", "Rida Shaikh", "Arjan Singh",
+              "Divisha Bahl", "Salil Mehta", "Ayushi Mishra", "Sameeha Roowalla", "Maanas", "Yahya Akil Sayed",
+              "Rishab Sharma", "Lavaniya Sharma", "Abheek Kapsime", "Dhruv Lagad", "Hussain", "Vivaan Bhaskaran",
+              "Marvellous", "Murtaza", "Jia", "Rida Khan", "Rushda Badgujar", "Zayan Firoz Kassim", "Mira", "Jas"]
+
+CANDIDATES_POSTS = {
+    "head_boy": ["Hanif Hasan Patel", "Aarush Verma"],
+    "head_girl": ["Diya Mahajan", "Sadhvi Kumar"],
+    "vice_head_boy": ["Shiven Onsker", "Shlok Rautwar"],
+    "vice_head_girl": ["Saanvi Trivedi", "Amaira Achwa"],
+    "sports_captain": ["Kyle Daniel", "Siddhant Dey"],
+    "vice_sports_captain": ["Rida Shaikh", "Arjan Singh"],
+    "cultural_head": ["Divisha Bahl", "Salil Mehta"],
+    "vice_cultural_head": ["Ayushi Mishra", "Sameeha Roowalla"],
+    "house_captain_spartans": ["Maanas", "Yahya Akil Sayed", "Rishab Sharma"],
+    "house_captain_samurais": ["Lavaniya Sharma", "Abheek Kapsime"],
+    "house_captain_trojans": ["Dhruv Lagad", "Hussain"],
+    "house_captain_knights": ["Vivaan Bhaskaran", "Marvellous"],
+    "vice_house_captain_spartans": ["Murtaza", "Jia"],
+    "vice_house_captain_samurais": ["Rida Khan"],
+    "vice_house_captain_trojans": ["Rushda Badgujar", "Zayan Firoz Kassim"],
+    "vice_house_captain_knights": ["Mira", "Jas"]
+}
--- website/views.py
+++ website/views.py
@@ -4,54 +4,9 @@ from .models import HeadBoy, HeadGirl, ViceHeadBoy, ViceHeadGirl, SportsCaptain,
     ViceHouseCaptainTrojans, HouseCaptainTrojans, HouseCaptainSamurais, ViceHouseCaptainSamurais, User
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-from sqlalchemy import inspect
-import json
+from .staticdata import *
 
 views = Blueprint('views', __name__)
-
-POSTS = [
-    "head_boy",
-    "head_girl",
-    "vice_head_boy",
-    "vice_head_girl",
-    "sports_captain",
-    "vice_sports_captain",
-    "cultural_head",
-    "vice_cultural_head",
-    "house_captain_spartans",
-    "house_captain_samurais",
-    "house_captain_trojans",
-    "house_captain_knights",
-    "vice_house_captain_spartans",
-    "vice_house_captain_samurais",
-    "vice_house_captain_trojans",
-    "vice_house_captain_knights",
-]
-
-CANDIDATES = ["Hanif Hasan Patel", "Aarush Verma", "Diya Mahajan", "Sadhvi Kumar", "Shiven Onsker", "Shlok Rautwar",
-              "Saanvi Trivedi", "Amaira Achwa", "Kyle Daniel", "Siddhant Dey", "Rida Shaikh", "Arjan Singh",
-              "Divisha Bahl", "Salil Mehta", "Ayushi Mishra", "Sameeha Roowalla", "Maanas", "Yahya Akil Sayed",
-              "Rishab Sharma", "Lavaniya Sharma", "Abheek Kapsime", "Dhruv Lagad", "Hussain", "Vivaan Bhaskaran",
-              "Marvellous", "Murtaza", "Jia", "Rida Khan", "Rushda Badgujar", "Zayan Firoz Kassim", "Mira", "Jas"]
-
-CANDIDATES_POSTS = {
-    "head_boy": ["Hanif Hasan Patel", "Aarush Verma"],
-    "head_girl": ["Diya Mahajan", "Sadhvi Kumar"],
-    "vice_head_boy": ["Shiven Onsker", "Shlok Rautwar"],
-    "vice_head_girl": ["Saanvi Trivedi", "Amaira Achwa"],
-    "sports_captain": ["Kyle Daniel", "Siddhant Dey"],
-    "vice_sports_captain": ["Rida Shaikh", "Arjan Singh"],
-    "cultural_head": ["Divisha Bahl", "Salil Mehta"],
-    "vice_cultural_head": ["Ayushi Mishra", "Sameeha Roowalla"],
-    "house_captain_spartans": ["Maanas", "Yahya Akil Sayed", "Rishab Sharma"],
-    "house_captain_samurais": ["Lavaniya Sharma", "Abheek Kapsime"],
-    "house_captain_trojans": ["Dhruv Lagad", "Hussain"],
-    "house_captain_knights": ["Vivaan Bhaskaran", "Marvellous"],
-    "vice_house_captain_spartans": ["Murtaza", "Jia"],
-    "vice_house_captain_samurais": ["Rida Khan"],
-    "vice_house_captain_trojans": ["Rushda Badgujar", "Zayan Firoz Kassim"],
-    "vice_house_captain_knights": ["Mira", "Jas"]
-}
 
 
 @views.route('/admin', methods=["GET", "POST"])
@@ -203,7 +158,7 @@ def vote(post):
 
     print(data)
 
-    return render_template('voting.html', candidates=data, post_name=post_name)
+    return render_template('voting.html', candidates=data, post_name=post_name, length=range(len(data)), imgs=[url_for('static', filename=f'/img/students/{c.lower().replace(" ", "-")}.jpeg') for c in data])
 
 
 @views.route('/thank-you', methods=["GET"])
