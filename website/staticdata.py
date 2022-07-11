1a6cc5f17ae41ada58e50e646d7e19486edf67a2
donnutdev <77726912+donnutdev@users.noreply.github.com>
2022-07-11 12:11:28 +0530

    final commit for code

--- website/staticdata.py
+++ website/staticdata.py
@@ -17,27 +17,27 @@ POSTS = [
     "vice_house_captain_knights",
 ]
 
-CANDIDATES = ["Hanif Hasan Patel", "Aarush Verma", "Diya Mahajan", "Sadhvi Kumar", "Shiven Onsker", "Shlok Rautwar",
+CANDIDATES = ["Hanif Hasan Patel", "Aarush Verma", "Diya Mahajan", "Sadhvi Kumar1", "Shiven Onskar", "Shlok Rautwar",
               "Saanvi Trivedi", "Amaira Achwa", "Kyle Daniel", "Siddhant Dey", "Rida Shaikh", "Arjan Singh",
-              "Divisha Bahl", "Salil Mehta", "Ayushi Mishra", "Sameeha Roowalla", "Maanas", "Yahya Akil Sayed",
-              "Rishab Sharma", "Lavaniya Sharma", "Abheek Kapsime", "Dhruv Lagad", "Hussain", "Vivaan Bhaskaran",
-              "Marvellous", "Murtaza", "Jia", "Rida Khan", "Rushda Badgujar", "Zayan Firoz Kassim", "Mira", "Jas"]
+              "Divisha Bahl", "Salil Mehta", "Aayushi Mishra", "Sameeha Roowalla", "Maanas Lohar", "Yahya Akil Sayed",
+              "Rishab Sharma", "Lavaniya Sharma", "Abheek Kapsime", "Dhruv Lagad", "Hussain Ali", "Vivaan Bhaskaran",
+              "Marvellous Olowo", "Murtuza", "Jia Varyani", "Rida Khan", "Zayan Firoz Kassim", "Mira", "Jas Varyani", "Alyssa Fernandes", "Hussain Harneswala"]
 
 CANDIDATES_POSTS = {
     "head_boy": ["Hanif Hasan Patel", "Aarush Verma"],
     "head_girl": ["Diya Mahajan", "Sadhvi Kumar"],
-    "vice_head_boy": ["Shiven Onsker", "Shlok Rautwar"],
+    "vice_head_boy": ["Shiven Onskar", "Shlok Rautwar"],
     "vice_head_girl": ["Saanvi Trivedi", "Amaira Achwa"],
     "sports_captain": ["Kyle Daniel", "Siddhant Dey"],
     "vice_sports_captain": ["Rida Shaikh", "Arjan Singh"],
     "cultural_head": ["Divisha Bahl", "Salil Mehta"],
-    "vice_cultural_head": ["Ayushi Mishra", "Sameeha Roowalla"],
-    "house_captain_spartans": ["Maanas", "Yahya Akil Sayed", "Rishab Sharma"],
+    "vice_cultural_head": ["Aayushi Mishra", "Sameeha Roowalla"],
+    "house_captain_spartans": ["Maanas Lohar", "Yahya Akil Sayed", "Rishab Sharma"],
     "house_captain_samurais": ["Lavaniya Sharma", "Abheek Kapsime"],
-    "house_captain_trojans": ["Dhruv Lagad", "Hussain"],
-    "house_captain_knights": ["Vivaan Bhaskaran", "Marvellous"],
-    "vice_house_captain_spartans": ["Murtaza", "Jia"],
-    "vice_house_captain_samurais": ["Rida Khan"],
-    "vice_house_captain_trojans": ["Rushda Badgujar", "Zayan Firoz Kassim"],
-    "vice_house_captain_knights": ["Mira", "Jas"]
+    "house_captain_trojans": ["Dhruv Lagad", "Hussain Ali"],
+    "house_captain_knights": ["Vivaan Bhaskaran", "Marvellous Olowo"],
+    "vice_house_captain_spartans": ["Murtuza", "Jia Varyani"],
+    "vice_house_captain_samurais": ["Rida Khan", "Alyssa Fernandes"],
+    "vice_house_captain_trojans": ["Zayan Firoz Kassim"],
+    "vice_house_captain_knights": ["Mira", "Jas Varyani", "Hussain Harneswala"]
 }
